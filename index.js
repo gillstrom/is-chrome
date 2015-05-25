27999fa8a90abd3cc53e3fea1e6e13f5aafdaf85
Andreas Gillström <andreasgillstrom@gmail.com>
2015-05-25 22:04:42 +0200

    Regex tweak

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = typeof navigator !== 'undefined' &&  /^(?!.*Chromium)(?=.*Chrome|.*CriOS).*/.test(navigator.userAgent);
+module.exports = typeof navigator !== 'undefined' && /^(?!.*Chromium|.*OPR|.*Opera)(?=.*Chrome|.*CriOS).*/.test(navigator.userAgent);
