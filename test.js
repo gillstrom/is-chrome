95c64a6d65a03bc13da0c7087bee4d04daae3c15
Aron Hafner <aronhafner@gmail.com>
2017-11-10 10:59:20 +0100

    Meta tweaks & es6ify (#1)

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-'use strict';
-var test = require('ava');
-var isChrome = require('./');
+import test from 'ava';
+import m from '.';
 
-test(function (t) {
+test('test different userAgents', t => {
 	t.plan(5);
 
-	t.assert(isChrome() === false);
-	t.assert(isChrome({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}) === true);
-	t.assert(isChrome({version: 42, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}) === true);
-	t.assert(isChrome({version: '42.0.2311.135', userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}) === true);
-	t.assert(isChrome({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36 OPR/29.0.1795.60'}) === false);
+	t.is(m({}), false);
+	t.is(m({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}), true);
+	t.is(m({version: 42, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}), true);
+	t.is(m({version: '42.0.2311.135', userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}), true);
+	t.is(m({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36 OPR/29.0.1795.60'}), false);
 });
